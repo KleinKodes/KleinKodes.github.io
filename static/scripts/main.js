7ae43a8ae6ef009a13e12924fc8577e5d3212374
KleinKodes <kleinharrigan@gmail.com>
2022-09-07 02:02:16 -0400

    Fixed bugs (mostly mobile ones) regarding improper handling of blank target links

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,7 +2,10 @@ function delay(URL, id, target = "_self") {
     if (target != "_self") target = "_blank";
     console.log(id);
     lower_card_hand(id);
-    setTimeout(function () { window.open(URL, target); if (target != "_self" && screen.width > 480) location.reload(); }, 3500);
+    setTimeout(function () {
+        var winref = window.open(URL, target); if (target != "_self" && screen.width > 480) location.reload();
+        if (target != "_self" && screen.width <= 480) { location.reload(); winref.location = URL }
+    }, 3500);
     console.log("here");
 
 
@@ -18,9 +21,11 @@ function lower_card_hand(id) {
 
 
 
+
     //maybe.hidden = true;
     overlay.style.visibility = "visible";
     overlay.append(haha);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 
     maybe.style.transition = "1s";
     maybe.style.transform = "translateY(100rem)";
